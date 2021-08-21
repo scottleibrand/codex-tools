b2445033d301705d354796854434641b10eca513
Scott Leibrand <scottleibrand@gmail.com>
2021-08-21 16:56:26 -0700

    fixes

--- auto-commenter.sh
+++ auto-commenter.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
-python3 auto-commenter.py
-diff -U 0 $1 $1.new > $1.patch
-cat $1.patch | egrep -B1 "^\+\s+\#\s" | egrep "@@|^\+\s+\#\s" > $1.patch.new
-patch $1 $1.patch.new
-
+# Process the filename provided in the first argument and convert it from a relative to an absolute path
+file=$(realpath $1)
+python3 auto-commenter.py $file
+diff -U 0 $file $file.new > $file.patch
+head -2 $file.patch > $file.patch.new
+cat $file.patch | egrep -B1 "^\+\s+\#\s" | egrep -A1 "@@ -\d+,0 \+\d+ @@" | egrep "@@|^\+\s+\#\s" >> $file.patch.new
+patch $file $file.patch.new
