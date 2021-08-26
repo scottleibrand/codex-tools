8c913cb559b7e2b686f0d813c94e14b14cc786a0
Scott Leibrand <scottleibrand@gmail.com>
2021-08-25 19:22:41 -0700

    working version

--- auto-docstring/auto-docstring.py
+++ auto-docstring/auto-docstring.py
@@ -4,17 +4,16 @@ Script to automatically add PEP 257 Google style doctrings to Python code
 
 Read in the code to be processed from a provided filename or from stdin. Read in the user’s GTP_API_KEY from an environment variable.
 
-Split out the preamble code before the first /^def / function definition.
-Split the code into chunks beginning with each /^def / function definition line.
+Split out the preamble code before the first /^\s+def / function definition.
+Split the code into chunks beginning with each /^\s+def / function definition line.
 For each non-preamble chunk:
  - Construct a Codex prompt consisting of:
   - the contents of autodocstring-example.txt
   - the code chunk
-  - the line '# A comprehensive PEP 257 Google style doctring, including a brief one-line summary of the function.'.
+  - the line '#autodoc: A comprehensive PEP 257 Google style doctring, including a brief one-line summary of the function.'.
   - the function definition line
-  - the string "    \"\"\"" without a trailing newline
   
- - Use Temperature 0, with a Stop sequence of "\"\"\"", to make Codex stop after it finishes generating the docstring.
+ - Use Temperature 0, with a Stop sequence of "#autodoc", to make Codex stop after it finishes generating the docstring.
 
  - Call the Codex API with the constructed prompt using the user’s GTP_API_KEY. API calls look like:
 
@@ -23,7 +22,7 @@ For each non-preamble chunk:
         "prompt": prompt,
         "max_tokens": 1500,
         "temperature": 0,
-        "stop": "\"\"\""
+        "stop": "#autodoc"
     })
     headers = {
         'Content-Type': 'application/json',
@@ -51,15 +50,14 @@ Functions called by main:
     Read in the code to be processed from a provided filename or from stdin.
 
 - get_code_chunks
-    Split the code into chunks beginning with each /^def / function definition line.
+    Split the code into chunks beginning with each /^\s+def / function definition line.
 
 - get_prompt
     Construct a Codex prompt consisting of:
     - the contents of autodocstring-example.txt
     - the code chunk
-    - the line '# A comprehensive PEP 257 Google style doctring, including a brief one-line summary of the function.'.
+    - the line '#autodoc: A comprehensive PEP 257 Google style doctring, including a brief one-line summary of the function.'.
     - the function definition line
-    - the string "    \"\"\"" without a trailing newline
 
 - get_response
     Call the Codex API with the constructed prompt using the user’s GTP_API_KEY. API calls look like:
@@ -69,7 +67,7 @@ Functions called by main:
         "prompt": prompt,
         "max_tokens": 1500,
         "temperature": 0,
-        "stop": "\"\"\""
+        "stop": "#autodoc"
     })
     headers = {
         'Content-Type': 'application/json',
@@ -80,19 +78,17 @@ Functions called by main:
 
     The response is json, and the output we want is in ['choices'][0]['text'].
 
-- format_response
-    Format the response text as a docstring by:
-    - adding triple " quotes immediately before it without a trailing newline
-    - adding triple " quotes on a new line after it.
-
-- remove_docstring
-    Remove any existing docstring in the original function code.
+- extract_function_code
+    Returns only the code of the function, without the funciton definition line or the docstring
 
 - replace_function_definition
-    Replace the original function definition with the function definition line, the docstring, and the original function code.
+    Replace the original function definition with the response and the original function code.
+
+- output_code
+    If the script was called with a filename, output the commented code to a .new file. Otherwise output it to stdout.
 
 - main
-    Call the functions in order.
+    Call output_code with the processed code.
 
 """
 
@@ -137,18 +133,18 @@ def get_code():
 
 def get_code_chunks(code):
     """
-    Split the code into chunks beginning with each /^def / function definition line.
+    Split the code into chunks beginning with each /^\s+def / function definition line.
 
     Parameters:
         code (str): The code to be processed.
 
     Returns:
-        chunks (list): The code split into chunks beginning with each /^def / function definition line.
+        chunks (list): The code split into chunks beginning with each /^\s+def / function definition line.
 
     """
     chunks = []
     for chunk in code.split('\n\n'):
-        if re.match(r'^def ', chunk):
+        if re.match(r'^\s+def ', chunk):
             chunks.append(chunk)
     return chunks
 
@@ -157,9 +153,8 @@ def get_prompt(code_chunk):
     Construct a Codex prompt consisting of:
     - the contents of autodocstring-example.txt
     - the code chunk
-    - the line '# A comprehensive PEP 257 Google style doctring, including a brief one-line summary of the function.'.
+    - the line '#autodoc: A comprehensive PEP 257 Google style doctring, including a brief one-line summary of the function.'.
     - the function definition line
-    - the string "    \"\"\"" without a trailing newline
 
     Parameters:
         code_chunk (str): A chunk of code.
@@ -171,9 +166,8 @@ def get_prompt(code_chunk):
     prompt = '\n\n'.join([
         open('autodocstring-example.txt').read(),
         code_chunk,
-        '# A comprehensive PEP 257 Google style doctring, including a brief one-line summary of the function.',
-        code_chunk.split('\n')[0],
-        '    \"\"\"'
+        '#autodoc: A comprehensive PEP 257 Google style doctring, including a brief one-line summary of the function.',
+        code_chunk.split('\n')[0]
     ])
     return prompt
 
@@ -186,7 +180,7 @@ def get_response(prompt):
         "prompt": prompt,
         "max_tokens": 1500,
         "temperature": 0,
-        "stop": "\"\"\""
+        "stop": "#autodoc"
     })
     headers = {
         'Content-Type': 'application/json',
@@ -208,7 +202,7 @@ def get_response(prompt):
         "prompt": prompt,
         "max_tokens": 1500,
         "temperature": 0,
-        "stop": "\"\"\""
+        "stop": "#autodoc"
     })
     headers = {
         'Content-Type': 'application/json',
@@ -217,59 +211,50 @@ def get_response(prompt):
     response = requests.post('https://api.openai.com/v1/engines/davinci-codex/completions', headers=headers, data=data)
     return response.json()['choices'][0]['text']
 
-def format_response(response):
+
+def extract_function_code(code_chunk):
     """
-    Format the response text as a docstring by:
-    - adding triple " quotes immediately before it without a trailing newline
-    - adding triple " quotes on a new line after it.
-
-    Parameters:
-        response (str): The response from the API.
-
-    Returns:
-        formatted_response (str): The response formatted as a docstring.
-
-    """
-    formatted_response = '\n\n'.join([
-        '    """',
-        response,
-        '    """',
-    ])
-    return formatted_response
-
-def remove_docstring(code_chunk):
-    """
-    Remove any existing docstring in the original function code.
+    Returns only the code of the function, without the funciton definition line or the docstring
 
     Parameters:
         code_chunk (str): A chunk of code.
 
     Returns:
-        code_chunk (str): A chunk of code with any existing docstring removed.
+        function_code (str): The code of the function.
 
     """
-    code_chunk = re.sub(r'^    \"\"\"[\s\S]*?\"\"\"', '', code_chunk)
-    return code_chunk
+    # Remove the function definition line
+    #print(code_chunk)
+    function_code = re.sub(r'^\s+def .+\n', '', code_chunk)
+    # Remove the first docstring
+    #print(function_code)
+    function_code = re.sub(r'""".*?"""', '', function_code, 1, flags=re.DOTALL)
+    #function_code = re.sub(r'\):\n*\s*""".*?"""', '\):\n', function_code, flags=re.DOTALL)
 
-def replace_function_definition(code_chunk, formatted_response):
+    #print(function_code)
+    return function_code
+
+
+def output_code(code):
     """
-    Replace the original function definition with the function definition line, the docstring, and the original function code.
+    If the script was called with a filename, output the commented code to a .new file. Otherwise output it to stdout.
 
     Parameters:
-        code_chunk (str): A chunk of code.
-        formatted_response (str): The response formatted as a docstring.
+        code (str): The code to be processed.
 
     Returns:
-        code_chunk (str): A chunk of code with the original function definition replaced by the function definition line, the docstring, and the original function code.
+        None
 
     """
-    code_chunk = re.sub(r'^def ', 'def ', code_chunk)
-    code_chunk = re.sub(r'\n    \"\"\"[\s\S]*?\"\"\"', formatted_response, code_chunk)
-    return code_chunk
+    if len(sys.argv) > 1:
+        with open(sys.argv[1] + '.new', 'w') as f:
+            f.write(code)
+    else:
+        print(code)
 
 def main():
     """
-    Call the functions in order.
+    Call output_code with the processed code.
 
     Parameters:
         None
@@ -283,10 +268,19 @@ def main():
     for chunk in chunks:
         prompt = get_prompt(chunk)
         response = get_response(prompt)
-        formatted_response = format_response(response)
-        code_chunk = remove_docstring(chunk)
-        code_chunk = replace_function_definition(code_chunk, formatted_response)
-        print(code_chunk)
+        #print(response)
+        function_code = extract_function_code(chunk)
+        #print(function_code)
+        new_chunk = '\n'.join([
+            response,
+            function_code
+        ])
+        # Remove any repeated blank lines
+        new_chunk = re.sub(r'\n+\s*\n+\s*\n+', '\n\n', new_chunk)
+
+        print(new_chunk)
+        code = code.replace(chunk, new_chunk)
+    output_code(code)
 
 if __name__ == '__main__':
     main()
